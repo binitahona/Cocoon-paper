f5c2cae2220bb57ebccbff8e462a46ac4080d35d
Binita Hona <bhona@mtu.edu>
2020-07-29 18:40:53 -0400

    systematic studies files added

---
+++ systematics/detector_systematics/reload_results-from-fits.py
@@ -0,0 +1,9 @@
+
+from threeML import *
+
+fits_file_name = ..  #name of the fits files from the results directory
+
+results_reloaded = load_analysis_results(fits_file_name)
+
+results_reloaded.display()
+
